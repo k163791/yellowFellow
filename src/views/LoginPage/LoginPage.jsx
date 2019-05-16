3a1cae0d1aa56eda2cfc0129470651ce00235670
k163791 <k163791@nu.edu.pk>
2019-05-16 14:21:03 +0500

    triggered

--- src/views/LoginPage/LoginPage.jsx
+++ src/views/LoginPage/LoginPage.jsx
@@ -190,11 +190,7 @@ class LoginPage extends React.Component {
                       />
                     </CardBody>
                     <CardFooter className={classes.cardFooter}>
-<<<<<<< HEAD
-                      <Button href='/waqarsFriend/#/comp' simple color="primary" size="lg">
-=======
-                      <Button href='//waqarsFriend/#/comp' simple color="primary" size="lg">
->>>>>>> 5b52cd26d0675161da4331dc006a072a33260e4c
+                      <Button href='//yellow-fellow/#/comp' simple color="primary" size="lg">
                         Get started
                       </Button>
                     </CardFooter>
